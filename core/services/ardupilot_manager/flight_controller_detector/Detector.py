2a3d5110f34113a01cb0de22e9b7c3a3e81e29e6
Vils0n <vils0nttm@gmail.com>
2024-08-19 11:55:36 +0300

    Update Detector.py

--- core/services/ardupilot_manager/flight_controller_detector/Detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/Detector.py
@@ -31,20 +31,9 @@ class Detector:
 
         def is_navigator_r5_connected() -> bool:
             try:
-                bus = SMBus(0)
-                AK09915_address = 0x0D
-                bus.read_byte_data(AK09915_address, 0)
-
                 bus = SMBus(1)
-                ADS1115_address = 0x48
-                bus.read_byte_data(ADS1115_address, 0)
-
                 BME280_address = 0x76
                 bus.read_byte_data(BME280_address, 0)
-
-                bus = SMBus(4)
-                PCA9685_address = 0x40
-                bus.read_byte_data(PCA9685_address, 0)
                 return True
             except Exception as error:
                 logger.warning(f"Navigator not detected: {error}")

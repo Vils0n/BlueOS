441b5308a972d18986d74d87674076f07491932f
Vils0n <vils0nttm@gmail.com>
2024-08-23 15:25:18 +0300

    Update Detector.py

--- core/services/ardupilot_manager/flight_controller_detector/Detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/Detector.py
@@ -32,18 +32,8 @@ class Detector:
         def is_navigator_r5_connected() -> bool:
             try:
                 bus = SMBus(1)
-                ADS1115_address = 0x48
-                bus.read_byte_data(ADS1115_address, 0)
-
-                AK09915_address = 0x0C
-                bus.read_byte_data(AK09915_address, 0)
-
-                BME280_address = 0x76
-                bus.read_byte_data(BME280_address, 0)
-
-                bus = SMBus(4)
-                PCA9685_address = 0x40
-                bus.read_byte_data(PCA9685_address, 0)
+                swap_multiplexer_address = 0x77
+                bus.read_byte_data(swap_multiplexer_address, 0)
                 return True
             except Exception as error:
                 logger.warning(f"Navigator not detected: {error}")
@@ -51,9 +41,20 @@ class Detector:
 
         def is_argonot_r1_connected() -> bool:
             try:
+                bus = SMBus(0)
+                AK09915_address = 0x0D
+                bus.read_byte_data(AK09915_address, 0)
+
                 bus = SMBus(1)
-                swap_multiplexer_address = 0x77
-                bus.read_byte_data(swap_multiplexer_address, 0)
+                ADS1115_address = 0x48
+                bus.read_byte_data(ADS1115_address, 0)
+
+                BME280_address = 0x76
+                bus.read_byte_data(BME280_address, 0)
+
+                bus = SMBus(4)
+                PCA9685_address = 0x40
+                bus.read_byte_data(PCA9685_address, 0)
                 return True
             except Exception as error:
                 logger.warning(f"Argonot not detected: {error}")

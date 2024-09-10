09e8e76c931386dba6abbc47506b7579c3da49c9
unknown <vils0nttm@gmail.com>
2024-09-10 17:48:57 +0300

    detector

--- core/services/ardupilot_manager/flight_controller_detector/Detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/Detector.py
@@ -31,7 +31,7 @@ class Detector:
 
               def is_navigator_r5_connected() -> bool:
             try:
-                bus = SMBus(0)
+                bus = SMBus(4)
                 PCA9685_address = 0x40
                 bus.read_byte_data(PCA9685_address, 0)
 
@@ -39,11 +39,11 @@ class Detector:
                 AK09915_address = 0x0C
                 bus.read_byte_data(AK09915_address, 0)
 
-                bus = SMBus(2)
+                bus = SMBus(1)
                 ADS1115_address = 0x48
                 bus.read_byte_data(ADS1115_address, 0)
 
-                bus = SMBus(3)
+                bus = SMBus(1)
                 BME280_address = 0x76
                 bus.read_byte_data(BME280_address, 0)
                 return True
